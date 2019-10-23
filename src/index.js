a83b61833d921af75af644b2c759f24c4d452459
Julio Jaramillo <juliodev@mx1.ibm.com>
2019-10-22 19:19:48 -0500

    Webpack added CSS, react and  HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+import './assets/styles/App.css';
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('app'))
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
